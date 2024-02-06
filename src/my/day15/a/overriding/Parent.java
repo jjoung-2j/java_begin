c0df42954d647681c65bfadb1338788b97d9a8e4
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-02-07 00:45:05 +0900

    [concept] overriding

---
+++ src/my/day15/a/overriding/Parent.java
@@ -0,0 +1,14 @@
+package my.day15.a.overriding;
+
+public class Parent {
+
+	// field
+	String id;
+	String passwd;
+	
+	// method
+	protected String info() {
+		return id + " , " + passwd;
+	}	// end of String info()
+	
+}
